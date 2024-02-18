16313126d27a0fe2abcf01440d9b98c37c23f01e
Hongseob Lee <sjun2918@naver.com>
2024-02-18 23:39:49 +0900

    ex09 : react 세팅

---
+++ ex09/my-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div>
+      <h1>상품 목록 페이지</h1>
+      <hr />
+      <div>
+        <Card/>
+        <Card/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ ex09/my-app/src/components/Card.js
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function Card() {
+  return (
+    <div>
+      <div className="product-card">
+        상품1
+      </div>
+    </div>
+  );
+}
+
+export default Card;
