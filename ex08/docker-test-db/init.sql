e81620776aaec526db8cb952c33cf98084bddbb3
Hongseob Lee <sjun2918@naver.com>
2024-02-18 15:26:02 +0900

    docker 경로 재설정

--- ex08/docker-test-db/init.sql
+++
@@ -1,7 +0,0 @@
-create tabel user_tb (
-    id int auto_increment primary key,
-    name varchar(50)
-) engine=innodb default charset=utf8mb4 collate=utf8mb4_unicode_ci;
-
-insert into user_tb (name) values ('lee');
-insert into user_tb (name) values ('kim');
